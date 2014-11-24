11df91b5e22e8ccca7051e28a88eacffc8b507aa
Adward-R <517908768@qq.com>
2014-11-24 15:46:58 +0800

    Enable Python script to convert data form

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Automatic-Rumor-Detection
 =========================
 
 SRTP Project
+
+ - read_data.py is adjusted to become a data preparer for libSVM
+ - read_data.py requires a file.xlsx for input, and a train.txt for output
--- read_data.py
+++ read_data.py
@@ -1,13 +1,14 @@
 
 import  xdrlib ,sys
 import xlrd
-def open_excel(file= 'file.xls'):
+def open_excel(file= 'file.xlsx'):
     try:
         data = xlrd.open_workbook(file)
         return data
     except Exception,e:
         print str(e)
-def excel_table_byindex(file= 'file.xls',colnameindex=0,by_index=0):
+def excel_table_byindex(file= 'file.xlsx',colnameindex=0,by_index=2):
+    fileHandle = open('train.txt','w')
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows
@@ -15,16 +16,24 @@ def excel_table_byindex(file= 'file.xls',colnameindex=0,by_index=0):
     colnames =  table.row_values(colnameindex)
     list =[]
     for rownum in range(1,nrows):
+        fileHandle.write('<1>')
+        row = table.row_values(rownum)
+        if row:
+             #app = {}
+            for i in (6,7,8,9,11,):
+            #for i in range(1,len(colnames)+1):
+                #app[colnames[i]] = row[i-1]
+                fileHandle.write(' <')
+                fileHandle.write(str(i))
+                fileHandle.write('>:<')
+                fileHandle.write(str(row[i-1]))
+                fileHandle.write('>')
+             #list.append(app)
+            fileHandle.write('\n')
+    return 0
+    #return list
 
-         row = table.row_values(rownum)
-         if row:
-             app = {}
-             for i in range(len(colnames)):
-                app[colnames[i]] = row[i]
-             list.append(app)
-    return list
-
-def excel_table_byname(file= 'file.xls',colnameindex=0,by_name=u'Sheet1'):
+def excel_table_byname(file= 'file.xlsx',colnameindex=0,by_name=u'Sheet1'):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
     nrows = table.nrows
@@ -41,12 +50,12 @@ def excel_table_byname(file= 'file.xls',colnameindex=0,by_name=u'Sheet1'):
 
 def main():
    tables = excel_table_byindex()
-   for row in tables:
-       print row
+   #for row in tables:
+   #    print row
 
-   tables = excel_table_byname()
-   for row in tables:
-       print row
+#   tables = excel_table_byname()
+#   for row in tables:
+#       print row
 
 if __name__=="__main__":
     main()
